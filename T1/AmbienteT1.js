2340814df00caa6dfc8274ffdcd0302c3c95eae2
V-Albino <victorbrandao007@gmail.com>
2025-05-23 14:32:39 -0300

    Commit 7 - Escadas Prototipo

--- T1/AmbienteT1.js
+++ T1/AmbienteT1.js
@@ -31,7 +31,8 @@ ground.rotation.x = -0.5 * Math.PI;
 scene.add(ground);
 
 // Geometria das areas
-const boxGeometry = new THREE.BoxGeometry(120, 120, 0.5);
+const areaGeometry = new THREE.BoxGeometry(120, 120, 0.5);
+const areaAzulGeometry = new THREE.BoxGeometry(120, 310, 0.5);
 
 // Cores das areas - Ainda vou buscar um jeito melhor de fazer isso
 const areaMaterial = [];
@@ -48,41 +49,62 @@ areaMaterial[3] = new THREE.MeshLambertMaterial({
     color:'rgb(100, 123, 255)'
 });
 
-// Cria as Areas e posiciona uma ao lado da outra
-let molde, area4; 
+// Cria o cubo que corta as areas
+let molde; 
 let areas = [];
-let postZ = -155;
-let cubeCSG, cubeMesh, auxCSG, objectCSG;
-cubeMesh = new THREE.Mesh(new THREE.BoxGeometry(30, 20, 2)) // cubo que vai cortar as areas (x, y, z)
-cubeMesh.position.set(0, 0, 0) // posição do cubo que vai cortar as areas
+let posZ = -155;
+let boxCSG, boxMesh, auxCSG, objectCSG;
+boxMesh = new THREE.Mesh(new THREE.BoxGeometry(30, 20, 2)) // cubo que vai cortar as areas (x, y, z)
+boxMesh.position.set(0, 0, 0) // posição do cubo que vai cortar as areas
+boxCSG = CSG.fromMesh(boxMesh); // passa o cubo para CSG
 
-
-molde = new THREE.Mesh(boxGeometry, areaMaterial[0]);
-molde.position.set(45, 0, 0);
-updateObject(molde); // atualiza a posição do objeto
-cubeCSG = CSG.fromMesh(cubeMesh); // passa o cubo para CSG
-auxCSG = CSG.fromMesh(molde);
-objectCSG = cortaArea(auxCSG, cubeCSG);
-
-for (let i=0; i<3; i++){
+// cria e corta as areas
+for(let i=0; i<=3; i++){
+    if(i<3){
+        molde = new THREE.Mesh(areaGeometry, areaMaterial[i]);
+        molde.position.set(45, 0, 0);
+    }
+    else if(i==3){
+        molde = new THREE.Mesh(areaAzulGeometry, areaMaterial[i]);
+        molde.position.set(-45, 0, 0);
+    }
+    updateObject(molde); // atualiza a posição do objeto
+    auxCSG = CSG.fromMesh(molde); // passa o molde para CSG
+    objectCSG = cortaArea(auxCSG, boxCSG);
     areas[i] = CSG.toMesh(objectCSG, new THREE.Matrix4()); 
-    areas[i].position.set(130, 5, postZ);
+    if(i == 3){
+        areas[i].position.set(-130, 5, 0);
+    }
+    else if(i < 3){
+        areas[i].position.set(130, 5, posZ);
+        criaEscada(115,posZ);
+    }
     areas[i].rotation.x = -0.5 * Math.PI;
     areas[i].material = areaMaterial[i];
-    postZ+=155;
+    posZ+=155;
 }
 
-// cria a Area 4
-const boxAzulGeometry = new THREE.BoxGeometry(120, 310, 0.5);
-area4 = new THREE.Mesh(boxAzulGeometry, areaMaterial[3]); 
-area4.position.set(-45, 0, 0);
-updateObject(area4); // atualiza a posição do objeto
-auxCSG = CSG.fromMesh(area4);  // cópia da area 4 em CSG
-objectCSG = cortaArea(auxCSG, cubeCSG); // corta a area 4
-area4 = CSG.toMesh(objectCSG, new THREE.Matrix4());
-area4.rotation.x = -0.5 * Math.PI;
-area4.material = areaMaterial[3];
-area4.position.set(-130, 5, 0);
+//criaEscada(115,0);
+
+function criaEscada(posX, posZ){
+    let degrau = [];
+    const alt = 5/8;
+    const comp = 30/8;
+    const degrauGeometry = new THREE.BoxGeometry(comp, alt, 20);
+    const degrauMaterial = new THREE.MeshLambertMaterial({
+        color:'rgb(96, 52, 255)'
+    });
+    for(let i=0; i<8; i++){
+        degrau = new THREE.Mesh(degrauGeometry, degrauMaterial);
+        degrau.position.set(i*comp + comp/2 + posX, i*alt + alt/2 , posZ)
+        scene.add(degrau);
+    }
+}
+
+// Adiciona as areas a cena
+areas.forEach(area => scene.add(area));
+//scene.add(cubeMesh)
+
 
 
 function updateObject(mesh)
@@ -97,19 +119,15 @@ function cortaArea(areaInteira, boxAuxiliar){
     return csgObject;
 }
 
-// Adiciona as areas a cena
-areas.forEach(area => scene.add(area));
-scene.add(area4);
-//scene.add(cubeMesh);
 
 // a rampa ainda será removida
-const rampGeometry = new THREE.PlaneGeometry(11, 10);
+const rampGeometry = new THREE.PlaneGeometry(30, 20);
 const rampMaterial = new THREE.MeshLambertMaterial({
 });
 const ramp = new THREE.Mesh(rampGeometry, rampMaterial);
 ramp.rotation.x = 1.5 * Math.PI;
 ramp.rotation.y = -Math.PI / 6;
-ramp.position.set(28.5, 2, 0);
+ramp.position.set(28.5, 0, 0);
 scene.add(ramp);
 
 // Paredes do Ambiente
@@ -192,7 +210,7 @@ function movementControls(key, value) {
 
 function moveAnimate(delta) {
     raycaster.ray.origin.copy(controls.getObject().position);
-    const isIntersectingGround = raycaster.intersectObjects([ground, areas[0], areas[1], areas[2], area4]).length > 0;
+    const isIntersectingGround = raycaster.intersectObjects([ground, areas[0], areas[1], areas[2], areas[3]]).length > 0;
     const isIntersectingRamp = raycaster.intersectObject(ramp).length > 0;
 
     if (moveForward) {
