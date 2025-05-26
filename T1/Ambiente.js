d95b1afbee4f391e10c367afa8eaa98d64da16fc
V-Albino <victorbrandao007@gmail.com>
2025-05-26 16:37:32 -0300

    Commit 15 - Rampas fixadas, escadas corrigidas

--- T1/Ambiente.js
+++ T1/Ambiente.js
@@ -1,20 +1,38 @@
 import * as THREE from 'three';
 import Stats from '../build/jsm/libs/stats.module.js';
-import {PointerLockControls} from '../build/jsm/controls/PointerLockControls.js';
-import {initRenderer,
-        initDefaultBasicLight,
-        onWindowResize} from "../libs/util/util.js";
+import { PointerLockControls } from '../build/jsm/controls/PointerLockControls.js';
+import {
+    initRenderer,
+    initDefaultBasicLight,
+    onWindowResize
+} from "../libs/util/util.js";
 
 import { CSG } from '../libs/other/CSGMesh.js'  // Constructive Solid Geometry(CSG), para fazer as Areas
 
 var stats = new Stats();          // To show FPS information
 var renderer = initRenderer("rgb(70, 150, 240)");    // View function in util/utils
 
+
 const scene = new THREE.Scene();
+const frustum = new THREE.Frustum();
+const cameraViewProjectionMatrix = new THREE.Matrix4();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(-5, 2, -5);
-camera.lookAt(new THREE.Vector3(0, 2, 0));
+camera.updateMatrixWorld();
+cameraViewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse);
+frustum.setFromProjectionMatrix(cameraViewProjectionMatrix);
+const controls = new PointerLockControls(camera, renderer.domElement);
+
+//Calculo do angulo da camera
+controls.getObject().position.set(10, 2, 1);
+const lookAtTarget = new THREE.Vector3(0.5, 2, 1);
+const direction = new THREE.Vector3().subVectors(lookAtTarget, controls.getObject().position).normalize();
+const angleY = Math.atan2(direction.x, direction.z);
+controls.getObject().rotation.y = angleY;
+scene.add(controls.getObject());
 scene.add(camera);
+
+
+window.camera = camera; //deixo a camera global pra testes
 
 const raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0).normalize(), 0, 2);
 initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
@@ -23,7 +41,7 @@ initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 //criando o chão
 const planeGeometry = new THREE.PlaneGeometry(500, 500, 5);
 const planeMaterial = new THREE.MeshLambertMaterial({
-    color:'rgb(249, 223, 184)'
+    color: 'rgb(249, 223, 184)'
 });
 const ground = new THREE.Mesh(planeGeometry, planeMaterial);
 ground.position.set(0, 0, 0);
@@ -37,80 +55,83 @@ const areaAzulGeometry = new THREE.BoxGeometry(120, 310, 10);
 // Cores das areas - Ainda vou buscar um jeito melhor de fazer isso
 const areaMaterial = [];
 areaMaterial[0] = new THREE.MeshLambertMaterial({
-    color:'rgb(155, 249, 134)'
+    color: 'rgb(155, 249, 134)'
 });
 areaMaterial[1] = new THREE.MeshLambertMaterial({
-    color:'rgb(210, 202, 55)'
+    color: 'rgb(210, 202, 55)'
 });
 areaMaterial[2] = new THREE.MeshLambertMaterial({
-    color:'rgb(255, 100, 100)'
+    color: 'rgb(255, 100, 100)'
 });
 areaMaterial[3] = new THREE.MeshLambertMaterial({
-    color:'rgb(100, 123, 255)'
+    color: 'rgb(100, 123, 255)'
 });
 
 // Cria o cubo que corta as areas
-let molde; 
+let molde;
 let areas = [];
 let posZ = -155;
 let boxCSG, boxMesh, auxCSG, objectCSG;
+let comp = 30 / 8;
 
-const rampGeometry = new THREE.PlaneGeometry(31.622, 20);
+const rampGeometry = new THREE.PlaneGeometry(38, 20);
 const rampMaterial = new THREE.MeshLambertMaterial({
 });
-const ramp = new THREE.Mesh(rampGeometry, rampMaterial);
+let ramp;
 
 boxMesh = new THREE.Mesh(new THREE.BoxGeometry(30, 20, 10)) // cubo que vai cortar as areas (x, y, z),
 boxMesh.position.set(0, 0, 0) // posição do cubo que vai cortar as areas
 boxCSG = CSG.fromMesh(boxMesh); // passa o cubo para CSG
 
 // cria e corta as areas
-for(let i=0; i<=3; i++){
-    if(i<3){
+for (let i = 0; i <= 3; i++) {
+    if (i < 3) {
         molde = new THREE.Mesh(areaGeometry, areaMaterial[i]);
         molde.position.set(45, 0, 0);
     }
-    else if(i==3){
+    else if (i == 3) {
         molde = new THREE.Mesh(areaAzulGeometry, areaMaterial[i]);
         molde.position.set(-45, 0, 0);
     }
     updateObject(molde); // atualiza a posição do objeto
     auxCSG = CSG.fromMesh(molde); // passa o molde para CSG
     objectCSG = cortaArea(auxCSG, boxCSG);
-    areas[i] = CSG.toMesh(objectCSG, new THREE.Matrix4()); 
-    if(i == 3){
+    areas[i] = CSG.toMesh(objectCSG, new THREE.Matrix4());
+    if (i == 3) {
         areas[i].position.set(-130, 5, 0);
+        comp = -1 * comp;
+        posZ = 0;
+        criaEscada(-115, posZ, comp);
     }
-    else if(i < 3){
+    else if (i < 3) {
         areas[i].position.set(130, 5, posZ);
-        criaEscada(115,posZ);
+        criaEscada(115, posZ, comp);
     }
     areas[i].rotation.x = -0.5 * Math.PI;
     areas[i].material = areaMaterial[i];
-    posZ+=155;
+    posZ += 155;
 }
 
-//criaEscada(115,0);
 
-function criaEscada(posX, posZ){
+function criaEscada(posX, posZ, comp) {
     let degrau = [];
-    const alt = 10/8;
-    const comp = 30/8;
-    const degrauGeometry = new THREE.BoxGeometry(comp, alt, 20);
+    const alt = 10 / 8;
+
+    const degrauGeometry = new THREE.BoxGeometry(30 / 8, alt, 20);
     const degrauMaterial = new THREE.MeshLambertMaterial({
-        color:'rgb(96, 52, 255)'
+        color: 'rgb(96, 52, 255)'
     });
-    for(let i=0; i<8; i++){
+    for (let i = 0; i < 8; i++) {
         degrau = new THREE.Mesh(degrauGeometry, degrauMaterial);
-        degrau.position.set(i*comp + comp/2 + posX, i*alt + alt/2 , posZ)
+        degrau.position.set(i * comp + comp / 2 + posX, i * alt + alt / 2, posZ);
         scene.add(degrau);
     }
-    
+    ramp = new THREE.Mesh(rampGeometry, rampMaterial);
     ramp.rotation.x = 1.5 * Math.PI;
-    ramp.rotation.y = -Math.PI / 12;
-    ramp.position.set(130, alt*4, posZ);
+    ramp.rotation.y = -Math.PI / 10;
+    ramp.position.set(126, 3.75, posZ);
+    //ramp.visible = false;
     scene.add(ramp);
-    
 }
 
 // Adiciona as areas a cena
@@ -119,13 +140,12 @@ areas.forEach(area => scene.add(area));
 
 
 
-function updateObject(mesh)
-{
-   mesh.matrixAutoUpdate = false;
-   mesh.updateMatrix();
+function updateObject(mesh) {
+    mesh.matrixAutoUpdate = false;
+    mesh.updateMatrix();
 }
 
-function cortaArea(areaInteira, boxAuxiliar){
+function cortaArea(areaInteira, boxAuxiliar) {
     let csgObject;
     csgObject = areaInteira.subtract(boxAuxiliar) // Subtrai parte da area
     return csgObject;
@@ -135,7 +155,7 @@ function cortaArea(areaInteira, boxAuxiliar){
 // Paredes do Ambiente
 const WallGeometry = new THREE.PlaneGeometry(500, 50);
 const wallMaterial = new THREE.MeshBasicMaterial({
-    color:'rgba(255, 140, 0, 0.65)'
+    color: 'rgba(255, 140, 0, 0.65)'
 });
 
 const walls = [];
@@ -155,7 +175,6 @@ walls[3].position.set(250, 25, 0);
 walls[3].rotation.y = Math.PI / -2;
 walls.forEach(wall => scene.add(wall));
 
-const controls = new PointerLockControls(camera, renderer.domElement);
 
 const blocker = document.getElementById('blocker');
 const instructions = document.getElementById('instructions');
@@ -241,16 +260,32 @@ function moveAnimate(delta) {
 }
 
 // Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 const clock = new THREE.Clock();
 render();
 function render() {
     stats.update();
+    // Atualiza o frustum da câmera
+    camera.updateMatrixWorld();
+    cameraViewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse);
+    frustum.setFromProjectionMatrix(cameraViewProjectionMatrix);
 
     if (controls.isLocked) {
         moveAnimate(clock.getDelta());
     }
+
+    
+    //A fazer (Thales) adicionar logica pra as areas que iram renderizar
+    // Exemplo de lógica para verificar se as áreas estão visíveis
+    // talvez alterar a visibilidade do que está fora do frustum
+    // Exemplo: areas.forEach(area => area.visible = frustum.intersectsObject(area));
+    areas.forEach(area => {
+        if (frustum.intersectsObject(area)) {
+            // Coloque aqui qualquer lógica que só deve rodar para áreas visíveis
+            // Exemplo: area.material.color.set('rgb(0,255,0)');
+        }
+    });
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
