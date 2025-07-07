a9b11d0e712ff15cf36441c5efc861eb3582b4fd
Thales <thales.batista@estudante.ufjf.br>
2025-07-07 16:26:15 -0300

    So dando uma ajustada

--- T1/inimigo.js
+++ T1/inimigo.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from '../build/jsm/loaders/GLTFLoader.js';
 
 export function adicionarInimigoCena(cena, caminhoGLB, posicao = { x: 0, y: 0, z: 0 }) {
-    const loader = new GLTFLoader();2
+    const loader = new GLTFLoader();
     loader.load(
         caminhoGLB,
         (gltf) => {
@@ -14,32 +14,30 @@ export function adicionarInimigoCena(cena, caminhoGLB, posicao = { x: 0, y: 0, z
 
             const boxSize = 7.57; // tamanho do lado do quadrado (ajuste para o seu modelo)
             const boxCenter = inimigo.position.clone();
-            const min = boxCenter.clone().add(new THREE.Vector3(-boxSize/2, -boxSize/2, -boxSize/2));
-            const max = boxCenter.clone().add(new THREE.Vector3(boxSize/2, boxSize/2, boxSize/2));
+            const min = boxCenter.clone().add(new THREE.Vector3(-boxSize / 2, -boxSize / 2, -boxSize / 2));
+            const max = boxCenter.clone().add(new THREE.Vector3(boxSize / 2, boxSize / 2, boxSize / 2));
             inimigo.userData.collisionBox = new THREE.Box3(min, max);
 
             const boxHelper = new THREE.Box3Helper(inimigo.userData.collisionBox, "red");
             cena.add(boxHelper);
 
-            inimigo.userData.boxHelper = boxHelper; 
-            inimigo.userData.state = "idle"; 
+            inimigo.userData.boxHelper = boxHelper;
+            inimigo.userData.state = "idle";
             inimigo.userData.detectionRadius = 100;
             inimigo.userData.moveType = "float";
             inimigo.userData.moveDirection = 1;
-            inimigo.userData.baseY = inimigo.position.y; 
+            inimigo.userData.baseY = inimigo.position.y;
             inimigo.userData.hp = 100;
             cena.add(inimigo);
-            console.log(gltf.animations);
-              // --- Animação ---
-              if (gltf.animations && gltf.animations.length > 0) {
+
+            // --- Animação ---
+            if (gltf.animations && gltf.animations.length > 0) {
                 const mixer = new THREE.AnimationMixer(inimigo);
-                // Procura a animação chamada "idle"
                 const idleClip = gltf.animations.find(clip => clip.name.toLowerCase() === "idle");
                 if (idleClip) {
                     const action = mixer.clipAction(idleClip);
                     action.play();
                 } else {
-                    // Se não achar "idle", toca a primeira animação mesmo
                     const action = mixer.clipAction(gltf.animations[0]);
                     action.play();
                 }
