2b59c664e4634dd9d422aacc4caea21cb70131c9
V-Albino <victorbrandao007@gmail.com>
2025-08-16 22:16:06 -0300

    chave azul coletavel

--- T1 copy/areaChave.js
+++ T1 copy/areaChave.js
@@ -36,8 +36,9 @@ export function criaChave(cor) {
 
     keyMesh = CSG.toMesh(keyCSG, new THREE.Matrix4());
     keyMesh.material = keyMaterial;
-    keyMesh.castShadow = true;
+    
     keyMesh.userData.isCollectable = true;
+    keyMesh.castShadow = true;
     keyMesh.receiveShadow = true;
 
     return keyMesh;
@@ -126,7 +127,7 @@ export function colisionChave(){
         new THREE.BoxGeometry(2, 20, 2),
         new THREE.MeshBasicMaterial({ 
             color: 0x00ff00,
-            visible: false,
+            visible: true,
             wireframe: true
         })
     );
